f2589ab2263628541a9c1e07c6fd010f15282e4f
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-03-04 14:37:03 -0500

    freemasonry

--- init.sh
+++ init.sh
@@ -60,8 +60,10 @@ sudo chmod 755 mizu
 echo ''
 info "Now installing dotnet..."
 echo ''
-bash -c "$(curl -fsSL https://dot.net/v1/dotnet-install.sh)"
-
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
+sudo apt install dotnet-sdk-6.0
 
 # Install everything else
 echo ''
