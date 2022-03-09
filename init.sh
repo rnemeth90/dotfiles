c19de9021f11af0b8bfc65aeb97ad44fd7749d01
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-03-09 16:53:30 -0500

    adding net-tools package

--- init.sh
+++ init.sh
@@ -71,7 +71,7 @@ info "Now installing everything else..."
 echo ''
 sudo apt install -y jq kubetail nmap nodejs golang ranger neofetch figlet kubectl helm  gnupg software-properties-common curl \
                     apt-transport-https ca-certificates curl terraform python3-pip nfs-common bash-completion speedtest-cli git \
-                    nikto dnsenum
+                    nikto dnsenum net-tools
 
 echo ''
 info "Now configuring git-completion..."
