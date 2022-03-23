5613981f939a27194fce486471434a4051d95ec2
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-03-23 12:52:20 -0400

    adding ngrok

--- init.sh
+++ init.sh
@@ -49,6 +49,15 @@ info "Now installing AzureCli sources..."
 echo ''
 sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+
+# Install azurecli
+echo ''
+info "Now installing Ngrok..."
+echo ''
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null &&
+              echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list &&
+              sudo apt update && sudo apt install ngrok
+
 echo ''
 info "Now installing mizu..."
 echo ''
