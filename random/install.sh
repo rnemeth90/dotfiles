3c29989729e74ae59e81619c8043b28f7231ae3c
Ryan Nemeth <ryannemeth@live.com>
2022-07-28 16:49:06 -0400

    disabling random installs

--- random/install.sh
+++ random/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Install everything else
 
-echo ''
-echo "Now installing everything else..."
-echo ''
-sudo apt install -y jq kubetail nmap nodejs golang ranger neofetch figlet gnupg software-properties-common curl \
-                    apt-transport-https ca-certificates curl python3-pip nfs-common bash-completion speedtest-cli git \
-                    wapiti dnsenum net-tools build-essential curl file 
+# echo ''
+# echo "Now installing everything else..."
+# echo ''
+# sudo apt install -y jq kubetail nmap nodejs golang ranger neofetch figlet gnupg software-properties-common curl \
+#                     apt-transport-https ca-certificates curl python3-pip nfs-common bash-completion speedtest-cli git \
+#                     wapiti dnsenum net-tools build-essential curl file 
