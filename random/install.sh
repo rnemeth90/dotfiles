4103a1225a689430ebef8110b6c40000b2c6f714
Ryan Nemeth <ryannemeth@live.com>
2022-07-28 16:42:56 -0400

    adding wapiti

--- random/install.sh
+++ random/install.sh
@@ -6,4 +6,4 @@ echo "Now installing everything else..."
 echo ''
 sudo apt install -y jq kubetail nmap nodejs golang ranger neofetch figlet gnupg software-properties-common curl \
                     apt-transport-https ca-certificates curl python3-pip nfs-common bash-completion speedtest-cli git \
-                    nikto dnsenum net-tools build-essential curl file 
+                    wapiti dnsenum net-tools build-essential curl file 
