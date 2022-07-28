2af03b648cafcd2770d8cc5cd9bdbca4e060d01c
Ryan Nemeth <ryannemeth@live.com>
2022-07-28 13:47:40 -0400

    updating script for mizu

--- mizu/install.sh
+++ mizu/install.sh
@@ -4,6 +4,8 @@
 echo ''
 info "Now installing mizu..."
 echo ''
+
+cd ~/.dotfiles/bin
 curl -Lo mizu https://github.com/up9inc/mizu/releases/latest/download/mizu_linux_amd64
-mv mizu ~/.dotfiles/bin
+mv mizu_linux_amd64 mizu
 sudo chmod 755 mizu
