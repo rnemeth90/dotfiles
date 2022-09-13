7912955d63a6508d77a9aa44ca2b182af6d23bdc
Ryan Nemeth <ryannemeth@live.com>
2022-09-13 11:00:13 -0400

    init commit of fedora branch

--- Functions/go.ps1
+++
@@ -1,7 +0,0 @@
-
-
-function init(){
-    go install golang.org/x/lint/golint@latest
-    go install golang.org/x/tools/cmd/goimports@latest
-
-}
--- Functions/terraform.ps1
+++
@@ -1,6 +0,0 @@
-Set-Alias tf 'terraform'
-
-function tfi { terraform init }
-function tfv { terraform validate }
-function tff { terraform fmt }
-function tfp { terraform plan }
