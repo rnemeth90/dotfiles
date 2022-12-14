2c2030aa4a8a816d840aedd44fe25c52a4b42bd6
Ryan Nemeth <ryannemeth@live.com>
2022-12-14 16:26:50 -0500

    fixing

--- os/apps.sh
+++ os/apps.sh
@@ -166,6 +166,7 @@ install_kubectl() {
   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
   sudo apt-get update
   sudo apt-get install -y kubectl
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
