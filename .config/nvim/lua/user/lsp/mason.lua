a831159137369152e3be5081448d8d501af2a542
ryan nemeth <ryannemeth@live.com>
2023-04-01 09:38:57 -0400

    golang autocompletion working

--- .config/nvim/lua/user/lsp/mason.lua
+++ .config/nvim/lua/user/lsp/mason.lua
@@ -1,12 +1,13 @@
 local servers = {
 	"sumneko_lua",
-	-- "cssls",
-	-- "html",
-	-- "tsserver",
+	"cssls",
+	"html",
+	"gopls",
+  "tsserver",
 	"pyright",
-	-- "bashls",
+	"bashls",
 	"jsonls",
-	-- "yamlls",
+  "yamlls",
 }
 
 local settings = {
