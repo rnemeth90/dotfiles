01c8165d91cf0ee4c6628bb90b1486e7f3733ac3
ryan nemeth <ryannemeth@live.com>
2023-04-21 20:19:01 -0400

    fix neovim config

--- .config/nvim/lua/user/neodev.lua
+++ .config/nvim/lua/user/neodev.lua
@@ -26,14 +26,3 @@ require("neodev").setup({
 
 -- then setup your lsp server as usual
 local lspconfig = require('lspconfig')
-
--- example to setup lua_ls and enable call snippets
-lspconfig.lua_ls.setup({
-  settings = {
-    -- Lua = {
-    --   completion = {
-    --     callSnippet = "Replace"
-    --   }
-    -- }
-  }
-})
