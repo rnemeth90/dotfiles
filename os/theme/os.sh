b9fb4928b0aec5ac48f8c000f041c2e620e4a270
ryan nemeth <ryannemeth@live.com>
2023-04-24 08:38:07 -0400

    picom transparency

--- os/theme/os.sh
+++ os/theme/os.sh
@@ -5,14 +5,10 @@ declare DOT=$HOME/dotfiles
 cd "$(dirname "${BASH_SOURCE[0]}")" &&
   . "$DOT/setup/utils.sh"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 install_oh_my_bash() {
   print_in_purple "\n • Installing Oh My bash \n\n"
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 }
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_fonts() {
   print_in_purple "\n • Install Fonts\n\n"
@@ -21,8 +17,6 @@ install_fonts() {
   sudo unzip ../Fira_Code_v6.2.zip -d ~/.local/share/fonts/
   fc-cache -fv
 }
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
   print_in_purple "\n • Start setting up OS theme...\n\n"
