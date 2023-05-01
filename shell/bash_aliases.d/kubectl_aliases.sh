9e619f76121883473ab567554d47943f66669c6a
ryan nemeth <ryannemeth@live.com>
2023-05-01 11:02:39 -0400

    add exports

--- shell/bash_aliases.d/kubectl_aliases.sh
+++ shell/bash_aliases.d/kubectl_aliases.sh
@@ -5,5 +5,3 @@ alias kgp='k get pods -o wide'
 alias kgn='k get ns'
 alias kgs='k get svc'
 alias kn='k config set-context --current --namespace'
-export KUBE_EDITOR=nano
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
