4c12f57239841295fc27ea3e5eba43b6bfd94459
ryan nemeth <ryannemeth@live.com>
2023-10-12 15:28:04 -0400

    init

--- os/mint/extensions_and_pkg_managers.sh
+++ os/mint/extensions_and_pkg_managers.sh
@@ -3,58 +3,49 @@
 declare DOT=$HOME/dotfiles
 
 cd "$(dirname "${BASH_SOURCE[0]}")" &&
-  . "$DOT/setup/utils.sh"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-add_flatpak_store_and_update() {
-  print_in_purple "\n • Add flatpak store and update\n\n"
-  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  flatpak update
-}
+    . "$DOT/setup/utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_snap() {
-  print_in_purple "\n • Installing snap\n\n"
-  sudo apt install -y snapd
-  sudo ln -s /var/lib/snapd/snap /snap
+    print_in_purple "\n • Installing snap\n\n"
+    sudo brew install snapd
+    sudo ln -s /var/lib/snapd/snap /snap
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_homebrew() {
-  print_in_purple "\n • Installing Homebrew \n\n"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    print_in_purple "\n • Installing Homebrew \n\n"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 install_nvm_node_yarn() {
-  print_in_purple "\n • Installing nvm, node and yarn. Use node LTS as default.\n\n"
-  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-  source ~/.bashrc
-  nvm install --lts
-  nvm use --lts
-  source ~/.bashrc
-  npm install --global yarn
+    print_in_purple "\n • Installing nvm, node and yarn. Use node LTS as default.\n\n"
+    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+    source ~/.bashrc
+    nvm install --lts
+    nvm use --lts
+    source ~/.bashrc
+    npm install --global yarn
 }
 
 install_npm() {
-  print_in_purple "\n • Installing npm \n\n"
-  sudo apt install npm -y 
+    print_in_purple "\n • Installing npm \n\n"
+    sudo brew install npm
 }
 
 install_cargo() {
-  print_in_purple "\n Installing cargo  \n\n"
-  curl https://sh.rustup.rs -sSf | sh -s -- -y
+    print_in_purple "\n Installing cargo  \n\n"
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
 }
 
 main() {
-  add_flatpak_store_and_update
-  install_snap
-  install_homebrew
-  install_nvm_node_yarn
-  install_npm
-  install_cargo
+    install_homebrew
+    install_npm
+    install_snap
+    install_nvm_node_yarn
+    install_cargo
 }
 
 main
--- os/mint/init_mint_setup.sh
+++ os/mint/init_mint_setup.sh
@@ -3,40 +3,20 @@
 declare DOT=$HOME/dotfiles
 
 cd "$(dirname "${BASH_SOURCE[0]}")" &&
-  . "$DOT/setup/utils.sh"
+    . "$DOT/setup/utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-set_hostname() {
-  print_in_purple "\n • Setting hostname... \n\n"
-  print_in_yellow "\nType in your hostname:\n"
-  read HOSTNAME
-  hostnamectl set-hostname $HOSTNAME
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-upgrade_apt() {
-  print_in_purple "\n • Upgrading... \n\n"
-  sudo apt upgrade -y
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-update_device_firmware() {
-  print_in_purple "\n • Updating device firmwares... \n\n"
-  sudo fwupdmgr get-devices
-  sudo fwupdmgr refresh --force
-  sudo fwupdmgr get-updates
-  sudo fwupdmgr update
-  print_in_yellow "\n !!! Don't restart yet if doing full setup! \n\n"
+upgrade_brew() {
+    print_in_purple "\n • Upgrading... \n\n"
+    sudo apt upgrade -y
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_xclip() {
-  print_in_purple "\n • Installing xclip for setup process... \n\n"
-  sudo apt install -y xclip
+    print_in_purple "\n • Installing xclip for setup process... \n\n"
+    sudo brew install xclip
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -46,10 +26,8 @@ install_xclip() {
 # ----------------------------------------------------------------------
 
 main() {
-  set_hostname
-  upgrade_apt
-  #update_device_firmware
-  install_xclip
+    upgrade_brew
+    install_xclip
 }
 
 main
