ec7845b719cafeb0f037416027babb8b9f9ed5a2
ryan nemeth <ryannemeth@live.com>
2023-12-13 16:20:08 -0500

    nvim config updates

--- .config/nvim/lua/user/go.lua
+++ .config/nvim/lua/user/go.lua
@@ -4,15 +4,6 @@
 --  return
 --end
 
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
-})
-
 -- Run gofmt + goimport on save
 local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -25,6 +25,7 @@ local options = {
   expandtab = true,                        -- convert tabs to spaces
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
+  softtabstop = 2,
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = false,                  -- set relative numbered lines
