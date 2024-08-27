82b8b59e357725b0374e821a7901b5251b203217
ryan nemeth <ryannemeth@live.com>
2024-08-26 20:59:54 -0400

    add tmux config

--- setup/create_symbolic_links.sh
+++ setup/create_symbolic_links.sh
@@ -1,5 +1,3 @@
-# This can be improved but I am lazy
-
 #!/bin/bash
 
 declare DOT=$HOME/dotfiles
@@ -7,12 +5,8 @@ declare DOT=$HOME/dotfiles
 cd "$(dirname "${BASH_SOURCE[0]}")" &&
     . "$DOT/setup/utils.sh"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 create_symlinks() {
-
     declare -a FILES_TO_SYMLINK=(
-
         "shell/bash_aliases.d"
         "shell/bash_aliases"
         "shell/bash_autocompletion"
@@ -36,44 +30,31 @@ create_symlinks() {
     local sourceFile=""
     local targetFile=""
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     for i in "${FILES_TO_SYMLINK[@]}"; do
-
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
         if [ ! -e "$targetFile" ]; then
-
             execute \
                 "ln -fs $sourceFile $targetFile" \
                 "$targetFile → $sourceFile"
-
         elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
             print_success "$targetFile → $sourceFile"
         else
-
             ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
             if answer_is_yes; then
-
                 rm -rf "$targetFile"
-
                 execute \
                     "ln -fs $sourceFile $targetFile" \
                     "$targetFile → $sourceFile"
-
             else
                 print_error "$targetFile → $sourceFile"
             fi
-
         fi
-
     done
-
 }
 
 create_config_symlinks() {
-
     # DO NOT INCLUDE THE LAST '/' IN THE PATHS BELOW!!
     declare -a FILES_TO_SYMLINK=(
         ".config/autostart"
@@ -86,50 +67,36 @@ create_config_symlinks() {
         ".config/polybar"
         ".config/picom"
         ".config/omb"
+        ".config/tmux"
     )
 
     local i=""
     local sourceFile=""
     local targetFile=""
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     for i in "${FILES_TO_SYMLINK[@]}"; do
-
         sourceFile="$(cd .. && pwd)/$i"
-        # targetFile="$HOME/.config/$(printf "%s" "$i" | sed 's:.*/::')/"
         targetFile="$HOME/.config/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
         if [ ! -e "$targetFile" ]; then
-
             execute \
                 "ln -fs $sourceFile $targetFile" \
                 "$targetFile → $sourceFile"
-
         elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
             print_success "$targetFile → $sourceFile"
         else
-
             ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
             if answer_is_yes; then
-
                 rm -rf "$targetFile"
-
                 execute \
                     "ln -fs $sourceFile $targetFile" \
                     "$targetFile → $sourceFile"
-
             else
                 print_error "$targetFile → $sourceFile"
             fi
-
         fi
-
     done
-
 }
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
     print_in_purple "\n • Create symbolic links\n\n"
