8afa856816b863503ad70bfa802847e8346c47e2
ryan nemeth <ryanthomasnemeth@gmail.com>
2024-12-09 21:57:10 -0500

    package updates

--- os/common/cargo.sh
+++ os/common/cargo.sh
@@ -36,6 +36,14 @@ main() {
         vivid
         stylua
         tree-sitter-cli
+        bottom # 17m, `btm`, Yet another cross-platform graphical process/system monitor (rust) - interactive with mouse and shortcuts
+        diskus # 3m, minimal, 3-10x faster alternative to `du -sh`
+        eza # 6m, maintained fork of exa; `eza --long --header --icons --git`
+        pueue # 17m, manage sequential/parallel long-running tasks, `pueued`, `pueue add ls; pueue add sleep 100; pueue; pueue log`
+        onefetch # 33m, like neofetch but stats for git repos, shows name, description, HEAD, version, languages, deps, authors, changes, contributors, commits, LOC, size, license
+        xh # 18m, faster httpie in Rust, but only subset of commands, ex: xh httpbin.org/post name=ahmed age:=24; xh :3000/users -> GET http://localhost:3000/users
+        tailspin # 11m, automatic log file highligher, supports numbers, dates, IP-addresses, UUIDs, URLs and more; pipe to `tspin`
+        spacer # 8m, insert spacers with datetime+duration when command output stops, default is 1s, `tail -f some.log | spacer --after 5` (only after 5s instead); `log stream --predicate 'process == "Google Chrome"' | spacer`; 'If you're the type of person that habitually presses enter a few times in your log tail to know where the last request ended and the new one begins, this tool is for you!' :)
     )
 
     for package in "${packages[@]}"; do
@@ -44,3 +52,4 @@ main() {
 }
 
 main
+
--- os/common/manual.sh
+++ os/common/manual.sh
@@ -12,6 +12,61 @@ check_command() {
     fi
 }
 
+install_google_chrome() {
+    print_in_green "\n • Installing Google Chrome \n\n"
+    if command -v google-chrome &>/dev/null; then
+        print_in_yellow "\n [✔] Google Chrome is already installed. Skipping...\n"
+    else
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo apt install ./google-chrome-stable_current_amd64.deb -y
+        rm google-chrome-stable_current_amd64.deb
+        print_in_green "\n [✔] Successfully installed Google Chrome.\n"
+    fi
+}
+
+install_azure_cli() {
+    print_in_green "\n • Installing Azure CLI \n\n"
+    if command -v az &>/dev/null; then
+        print_in_yellow "\n [✔] Azure CLI is already installed. Skipping...\n"
+    else
+        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+        print_in_green "\n [✔] Successfully installed Azure CLI.\n"
+    fi
+}
+
+install_nodejs_and_typescript() {
+    print_in_green "\n • Installing Node.js and TypeScript \n\n"
+    if command -v node &>/dev/null; then
+        print_in_yellow "\n [✔] Node.js is already installed. Skipping...\n"
+    else
+        curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+        sudo apt install -y nodejs
+        print_in_green "\n [✔] Successfully installed Node.js.\n"
+    fi
+
+    if command -v tsc &>/dev/null; then
+        print_in_yellow "\n [✔] TypeScript is already installed. Skipping...\n"
+    else
+        sudo npm install -g typescript
+        print_in_green "\n [✔] Successfully installed TypeScript.\n"
+    fi
+}
+
+install_vscode() {
+    print_in_green "\n • Installing Visual Studio Code \n\n"
+    if command -v code &>/dev/null; then
+        print_in_yellow "\n [✔] Visual Studio Code is already installed. Skipping...\n"
+    else
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+        sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+        sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+        sudo apt update
+        sudo apt install code -y
+        rm packages.microsoft.gpg
+        print_in_green "\n [✔] Successfully installed Visual Studio Code.\n"
+    fi
+}
+
 install_kubeshark() {
     print_in_green "\n • Installing kubeshark \n\n"
     if command -v kubeshark &>/dev/null; then
@@ -43,20 +98,33 @@ install_golangci_lint() {
     fi
 }
 
-install_bash_completion() {
-    print_in_green "\n • Installing bash completion for Git \n\n"
-    local git_completion="$HOME/.git-completion.bash"
-
-    if [[ -f "$git_completion" ]]; then
-        print_in_yellow "\n [✔] Git bash completion is already installed. Skipping...\n"
+install_yq() {
+    print_in_green "\n • Installing yq \n\n"
+    if command -v yq &>/dev/null; then
+        print_in_yellow "\n [✔] yq is already installed. Skipping...\n"
     else
-        if curl -o "$git_completion" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash; then
-            echo "[ -f ~/.git-completion.bash ] && . ~/.git-completion.bash" >>"$HOME/.bashrc"
-            print_in_green "\n [✔] Successfully installed Git bash completion.\n"
-        else
-            print_in_red "\n [✖] Failed to install Git bash completion.\n"
-            exit 1
-        fi
+        sudo snap install yq
+        print_in_green "\n [✔] Successfully installed yq.\n"
+    fi
+}
+
+install_terraform() {
+    print_in_green "\n • Installing Terraform \n\n"
+    if command -v terraform &>/dev/null; then
+        print_in_yellow "\n [✔] Terraform is already installed. Skipping...\n"
+    else
+        sudo snap install terraform --classic
+        print_in_green "\n [✔] Successfully installed Terraform.\n"
+    fi
+}
+
+install_speedtest() {
+    print_in_green "\n • Installing Speedtest-cli \n\n"
+    if command -v speedtest &>/dev/null; then
+      curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+      sudo apt-get install speedtest
+    else
+        print_in_green "\n [✔] Speedtest-cli already installed.\n"
     fi
 }
 
@@ -65,9 +133,16 @@ main() {
     check_command "sh"
     check_command "go"
 
+    install_google_chrome
+    install_azure_cli
+    install_nodejs_and_typescript
+    install_vscode
     install_kubeshark
     install_golangci_lint
-    install_bash_completion
+    install_yq
+    install_terraform
+    install_speedtest
 }
 
 main
+
