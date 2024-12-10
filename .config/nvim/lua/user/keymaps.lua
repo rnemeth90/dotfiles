81ab03212b78139b7e99aab5f232fc26c76dc5ef
ryan nemeth <ryanthomasnemeth@gmail.com>
2024-12-10 15:49:29 -0500

    neovim updates

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -20,11 +20,11 @@ vim.g.maplocalleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- Better window pane navigation
+keymap("n", "<C-h>", "<C-w>h", opts) -- left
+keymap("n", "<C-j>", "<C-w>j", opts) -- down
+keymap("n", "<C-k>", "<C-w>k", opts) -- up
+keymap("n", "<C-l>", "<C-w>l", opts) -- right
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
@@ -39,6 +39,8 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts) -- Shift visual selected line down
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts) -- Shift visual selected line up
 
 -- Insert --
 -- Press jk fast to exit insert mode
