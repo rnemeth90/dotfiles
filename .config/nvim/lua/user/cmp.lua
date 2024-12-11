4d126573b286113f81857855754f462ed1f07ce1
ryan nemeth <ryanthomasnemeth@gmail.com>
2024-12-11 06:52:55 -0500

    fixing cmp

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -8,11 +8,11 @@ if not snip_status_ok then
   return
 end
 
--- require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+  local col = vim.fn.col(".") - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
 --   פּ ﯟ   some other good icons
@@ -45,26 +45,33 @@ local kind_icons = {
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
-cmp.setup {
+cmp.setup({
+  -- snippet engine to use
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
-  mapping = {
+  mapping = { -- keymaps for cmp
     ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping {
+    ["<C-s>"] = cmp.mapping(cmp.mapping.complete({
+      config = {
+        sources = {
+          { name = "luasnip" },
+        },
+      },
+    })),
+    -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
-    },
+    }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -95,6 +102,7 @@ cmp.setup {
     }),
   },
   formatting = {
+    expandable_indicator = true,
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       -- Kind icons
@@ -128,4 +136,4 @@ cmp.setup {
     ghost_text = true,
     native_menu = false,
   },
-}
+})
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -69,16 +69,17 @@ return packer.startup(function(use)
   use({ "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" })
   use({ "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" })
   use({ "lunarvim/colorschemes" })
+  use({ "catppuccin/nvim", as = "catppuccin" })
 
   -- Completions
-  use({ "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" })        -- The completion plugin
-  use({ "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" })      -- buffer completions
-  use({ "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" })        -- path completions
+  use({ "hrsh7th/nvim-cmp" })                                                             -- The completion plugin
+  use({ "hrsh7th/cmp-buffer" })                                                           -- buffer completions
+  use({ "hrsh7th/cmp-path" })                                                             -- path completions
+  use({ "hrsh7th/cmp-nvim-lsp" })                                                         -- completions for lsp
+  use({ "hrsh7th/cmp-nvim-lua" })                                                         -- completions for lua
   use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
-  use({ "hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" })    -- completions for lsp
-  use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
 
-  -- Snippets, required for cmp
+  -- Snippets, required for cmp (completions)
   use({
     "L3MON4D3/LuaSnip",
     tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
