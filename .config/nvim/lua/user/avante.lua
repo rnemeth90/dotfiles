9ddfd2136137bb8953353a9763b19733aac98aa9
ryan nemeth <ryanthomasnemeth@gmail.com>
2025-03-26 12:58:35 -0400

    pre-packer migration to lazy

---
+++ .config/nvim/lua/user/avante.lua
@@ -0,0 +1,6 @@
+local status_ok, avante = pcall(require, "avante")
+if not status_ok then
+  return
+end
+
+avante.setup()
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -47,7 +47,7 @@ return packer.startup(function(use)
   use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
   use({ "numToStr/Comment.nvim" })
   use({ "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" })
-  use({ "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" })
+  -- use({ "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" })
   use({ "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }) -- Tree navigation to replace netrw
   use({ "akinsho/bufferline.nvim" })
   use({ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" })
@@ -130,11 +130,26 @@ return packer.startup(function(use)
   use("karb94/neoscroll.nvim")
 
   -- openai stuff
+  -- Avante
+  -- copilot
   use("MunifTanjim/nui.nvim")
   use("Bryley/neoai.nvim")
+  use 'stevearc/dressing.nvim'
+  use 'MeanderingProgrammer/render-markdown.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'HakonHarnes/img-clip.nvim'
+  -- use("github/copilot.vim")
+  use( "zbirenbaum/copilot.lua" )
 
-  -- copilot
-  use("github/copilot.vim")
+  -- Avante.nvim with build process
+  use {
+    'yetone/avante.nvim',
+    branch = 'main',
+    run = 'make',
+    config = function()
+      require('avante').setup()
+    end
+  }
 
   -- helm
   use("towolf/vim-helm")
