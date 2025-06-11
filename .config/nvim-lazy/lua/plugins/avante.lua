576b6ab162f7121172e2f51858bfc46f32e44189
ryan nemeth <ryanthomasnemeth@gmail.com>
2025-06-11 14:32:12 -0400

    lazy conf

--- .config/nvim-lazy/lua/plugins/avante.lua
+++ .config/nvim-lazy/lua/plugins/avante.lua
@@ -11,8 +11,8 @@ return {
         endpoint = "https://api.openai.com/v1",
         model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
         timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        -- temperature = 0,
-        -- max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        temperature = 0,
+        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
         --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
       }
     },
